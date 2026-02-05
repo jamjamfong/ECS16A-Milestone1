137d3ef6c7b98c7627da11c5e3d61af154ff7d0d
Sabrina Cheung <sabrina.c.cheung@gmail.com>
2026-02-05 00:00:24 -0800

    finish page class

--- lstore/page.py
+++ lstore/page.py
@@ -6,9 +6,16 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
-        pass
+        # Each record is 8 bytes (assuming integer size), so we can store 4096 / 8 = 512 records
+        # Checks if the current number of records is less than the maximum capacity
+        return self.num_records < (len(self.data) // 8)
 
     def write(self, value):
+        """
+        Store an integer value in the page
+        Converts the integer to bytes and stores it in the bytearray at the correct position
+        """
         self.num_records += 1
-        pass
+        offset = (self.num_records - 1) * 8
+        self.data[offset:offset + 8] = value.to_bytes(8, byteorder ='little', signed=True)
 
