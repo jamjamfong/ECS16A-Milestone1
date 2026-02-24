87a3f9322e411c939e1ec02b947acabc283184c0
jamjamfong <jamiefongg@gmail.com>
2026-02-23 21:52:21 -0800

    index

--- lstore/index.py
+++ lstore/index.py
@@ -1,6 +1,7 @@
 """
 A data strucutre holding indices for various columns of a table. Key column should be indexd by default, other columns can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
 """
+from BTrees.OOBTree import OOBTree
 
 class Index:
 
@@ -14,7 +15,7 @@ class Index:
     def locate(self, column, value):
       if self.indices[column] is None or value not in self.indices[column]:
           return []
-      return self.indices[column][value]
+      return self.indices[column].get(value, [])
     """
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
     """
@@ -22,34 +23,42 @@ class Index:
       search_results = []
       if self.indices[column] is None:
           return search_results
-      for value in self.indices[column]:
-          if begin <= value <= end:
-              search_results.extend(self.indices[column][value])
-              
+      for rid_list in self.indices[column].values(begin, end):
+          search_results.extend(rid_list)
       return search_results
     """
     # optional: Create index on specific column
     """
     def create_index(self, column_number):
         if self.indices[column_number] is None:
-            self.indices[column_number] = {}
+            self.indices[column_number] = OOBTree()
+        for rid, location in self.table.page_directory.items():
+            value = self.table.get_latest_value(rid, column_number)
+            self.add_to_index(column_number, value, rid)
+
+    def add_to_index(self, column, value, rid):
+        if self.indices[column] is None:
+            return
+        if value not in self.indices[column]:
+            self.indices[column][value] = []
+        if rid not in self.indices[column][value]:
+            self.indices[column][value].append(rid)
+
     """
     # optional: Drop index of specific column
     """
     def drop_index(self, column_number):
-      self.indices[column_number] = None
+        if column_number != self.table.key:
+            self.indices[column_number] = None
       
     def insert_key(self, value, rid):
         column = self.table.key
-        
         if self.indices[column] is None:
             self.create_index(column)
-        
         if value not in self.indices[column]:
             self.indices[column][value] = []
-        
         self.indices[column][value].append(rid)
         
     def remove_key(self, column, value):
-        if self.indices[column] is not None and value in self.indices[column]:
-            del self.indices[column][value]
+        if rid in self.indices[column][value]:
+            self.indices[column][value].remove(rid)
